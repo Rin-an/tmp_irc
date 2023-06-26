aff39901190278d5a2054dc96bf20be9e4ebe424
Sahar Sadiki <ssadiki@e2r2p12.1337.ma>
2023-06-26 23:21:07 +0100

    adding topic command

--- Channel.hpp
+++ Channel.hpp
@@ -1,19 +1,30 @@
 #ifndef CHANNEL_HPP
 #define CHANNEL_HPP
+#define CHANNEL_CHARS "&#+|"
+#define CHANNEL_MODS "klito"
 #include "client.hpp"
 #include <deque>
 
 class Server;
-class User;
 class Client;
 class Channel{
-	std::string	name;
-	std::string	mode;
-	public:
-		Channel();
-		~Channel();
-		std::deque<Server *> op;
-		std::deque<Server *> users;
-		//std::deque<Server *> 
+    std::string name;
+    std::string key;
+    std::string topic;
+    std::string mode;
+    public:
+        std::deque<Server *> op;
+        std::deque<Server *> users;
+        Channel();
+        Channel(const std::string&);
+        ~Channel();
+        const std::string&  getName(void) const;
+        const std:string&   getTopic(void) const;
+        const std::string&  getMode(void) const;
+        void                setName(std::string&);
+        void                setTopic(std::string&);
+        void                setMode(std::string&);
+        void                setKey(std::string&);
+        int                 topic_cmd(std::string);
 };
 #endif
